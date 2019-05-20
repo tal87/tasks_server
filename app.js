71876ef842d93c7ff627bf71f8e129099679ccfd
tal87 <talbsho@gmail.com>
2019-05-20 07:03:32 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const app = require("express")();
+const mongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let url =
+  "mongodb://uj2rrk4jfrv4c4c416es:RenWBG0XFUc756E8Mld9@bjgtlg6ithkz39l-mongodb.services.clever-cloud.com:27017/bjgtlg6ithkz39l";
+mongoClient.connect(url, (err, db) => {
+  if (err) throw err;
+  let dbo = db.db("bjgtlg6ithkz39l");
+
+  app.get("/api/tasks", (req, res) => {
+    res.setHeader("access-control-allow-origin", "*");
+    res.contentType("application/json");
+    dbo.collection("tasks", (err, collection) => {
+      if (err) {
+        throw err;
+      }
+
+      collection.find().toArray((err, docs) => {
+        if (!err) {
+          res.end(JSON.stringify(docs));
+        }
+      });
+    });
+  });
+
+  app.options("/api/tasks", (req, res) => {
+    res.setHeader("access-control-allow-origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.end();
+  });
+
+  app.post("/api/tasks", (req, res) => {
+    res.setHeader("access-control-allow-origin", "*");
+    dbo.collection("tasks", (err, collection) => {
+      if (err) {
+        throw err;
+      }
+
+      collection.insert(
+        { subject: req.body.subject, text: req.body.text },
+        (err, result) => {
+          if (!err) {
+            console.log(result);
+            res.end("done");
+          }
+        }
+      );
+    });
+  });
+
+  app.listen(5000);
+});
